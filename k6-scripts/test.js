f65be56bae97af66455dae2949359255dc8ffa90
SALIGAMA <82266368+SALIGAMA@users.noreply.github.com>
2026-07-14 13:22:55 +0530

    Create      test.js

---
+++ k6-scripts/test.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  vus: 5,
+  duration: '10s',
+};
+
+export default function () {
+  const res = http.get('https://test.k6.io');
+
+  check(res, {
+    'Status is 200': (r) => r.status === 200,
+  });
+}
